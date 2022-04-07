2fce7e95698542a7e9ea02b6b02f28e726a45140
zDany01 <85403430+zDany01@users.noreply.github.com>
2022-04-07 17:09:34 +0200

    App Setup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSys32Delete
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
